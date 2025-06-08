536893d53a3841b66ac396a918811287c516c19c
RestedWicked <wicked@radiantware.xyz>
2025-06-07 22:52:59 -0700

    [Refactor] Using Clap for the cli

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,50 @@
-use std::env;
-
+use clap::{Parser, Subcommand};
 use toby_ssg::{create_note, init, serve::serve, ssg::render, validate_working_directory};
+
+/// Toby SSG
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None, arg_required_else_help(true))]
+#[clap(disable_help_subcommand = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Initialize a directory for usage with Toby SSG.
+    Init,
+    /// Serve prerendered site files.
+    Serve,
+    /// Render markdown files.
+    Render,
+    /// Create new markdown file.
+    Note {
+        /// File name for new note. Example: cat_notes
+        name: Option<String>,
+    },
+    /// Validate working directory.
+    Validate,
+}
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    let cli = Cli::parse();
 
-    if args.len() == 1 {
-        validate_working_directory();
-        return;
-    }
-
-    let query = &args[1];
-    match query.as_str() {
-        "init" => init(),
-        "serve" => serve().await,
-        "render" => {
+    match &cli.command {
+        Some(Commands::Init) => init(),
+        Some(Commands::Serve) => serve().await,
+        Some(Commands::Render) => {
             let _ = render().await;
-        },
-        "note" => {
-            if args.len() == 3 {
-                create_note(&args[2]);
+        }
+        Some(Commands::Note { name }) => {
+            if let Some(name) = name {
+                create_note(&name)
             } else {
                 create_note("new_note")
             }
         }
-        _ => panic!("Not a valid argument {}", { query }),
+        Some(Commands::Validate) => validate_working_directory(),
+        None => (),
     }
 }
